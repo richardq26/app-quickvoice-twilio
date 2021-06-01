d37775c9497c0a23d40b9c54113770e6b8569786
Richard <richard.sw268@gmail.com>
2021-06-01 12:15:19 -0500

    Personalizando twiml

--- controller/twilio.controller.js
+++ controller/twilio.controller.js
@@ -1,12 +1,13 @@
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 
-exports.quickcall = (req, res) => {
+exports.quickcall = async (req, res) => {
   const client = require("twilio")(accountSid, authToken);
-  console.log(req.body.number)
+  const message = req.body.message;
+  console.log(req.body);
   client.calls
     .create({
-      url: "http://demo.twilio.com/docs/voice.xml",
+      twiml: `<Response><Say voice="alice" language="es-MX">${message}</Say></Response>`,
       to: req.body.number,
       from: process.env.FROM_NUMBER,
     })
@@ -26,3 +27,35 @@ exports.quickcall = (req, res) => {
 //   twiml.say("Hello from your pals at Twilio! Have fun.");
 //   res.status(200).json(twil.toString());
 // };
+
+exports.sendSMS = async (req, res) => {
+  const client = require("twilio")(accountSid, authToken);
+  client.messages
+    .create({
+      to: req.body.number,
+      from: process.env.FROM_NUMBER,
+      body: req.body.text,
+    })
+    .then((message) => {
+      console.log(`Message ${message.sid} sent!`);
+      res.status(200).json({ msg: "Mensaje enviado!", sid: message.sid });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).json(error);
+    });
+};
+
+exports.getVoiceMessage = async (req, res) => {
+  const VoiceResponse = require("twilio").twiml.VoiceResponse;
+  const response = new VoiceResponse();
+  response.say(
+    {
+      voice: "woman",
+      language: "es",
+    },
+    "Holaaa"
+  );
+  console.log(response.toString());
+  return response.toString();
+};
--- routes/twilio.routes.js
+++ routes/twilio.routes.js
@@ -1,7 +1,11 @@
 const{Router}=require("express");
-const { quickcall } = require("../controller/twilio.controller");
+const { quickcall, sendSMS, getVoiceMessage } = require("../controller/twilio.controller");
 const router= Router();
-
+const path = require("path")
 router.post("/quickcall", quickcall);
-
+router.post("/sendSMS", sendSMS )
+// router.get("/message", getVoiceMessage)
+// router.get("/voiceXML.xml", (req,res)=>{
+//     res.sendFile(path.join(__dirname,"../views/messageVoice.xml"))
+// }), 
 module.exports = router;
